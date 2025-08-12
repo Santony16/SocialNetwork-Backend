5bb5ff37270fe72db5e32a4cefaff30213b05460
Santony16 <anthonysancho41@gmail.com>
2025-08-11 19:53:37 -0600

    User data model:
    User schema definition with validation for authentication system

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes, literal } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password_hash: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  two_factor_enabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  },
+  two_factor_secret: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+}, {
+  tableName: 'users',
+  schema: 'dbo',
+  timestamps: false
+});
+
+module.exports = User;
